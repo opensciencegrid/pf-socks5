ddc8291b27ee2d4ba8503f881651efc10836171c
Soichi Hayashi <soichih@gmail.com>
2015-01-09 14:48:53 +0000

    added update.cron

--- test/test_socks5.sh
+++ test/test_socks5.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "setting up iptables for LHCONE hosts"
-sudo /sbin/iptables -t nat -N REDSOCKS
-sudo /sbin/iptables -t nat -A REDSOCKS -p tcp -d lhcone.test.manlan.internet2.edu -j REDIRECT --to-ports 12380
-sudo /sbin/iptables -t nat -A OUTPUT -p tcp -j REDSOCKS
+#echo "setting up iptables for LHCONE hosts"
+#sudo /sbin/iptables -t nat -N REDSOCKS
+#sudo /sbin/iptables -t nat -A REDSOCKS -p tcp -d lhcone.test.manlan.internet2.edu -j REDIRECT --to-ports 12380
+#sudo /sbin/iptables -t nat -A OUTPUT -p tcp -j REDSOCKS
 
-echo "listing nat table"
-sudo /sbin/iptables -L -t nat
+#echo "listing nat table"
+#sudo /sbin/iptables -L -t nat
 
 echo "test accessing via socks5"
 curl -k --socks5 localhost:1080 https://lhcone.test.manlan.internet2.edu/toolkit/?format=json
