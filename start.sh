89a8f10a88f901f81d99a5f746674e5ccca8d6dd
Soichi Hayashi <soichih@gmail.com>
2015-01-14 18:31:26 +0000

    renamed pid file

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 echo "starting socks5 proxy"
 ssh  -o StrictHostKeyChecking=no -fqND localhost:1080 -i secrets/sock5_proxy.id_rsa root@maddash.aglt2.org
-echo $! > /var/run/redsocks.pid
+echo $! > /var/run/pf-socks5-ssh.pid
 
 echo "starting redsocks (using default config path ./redsocks.conf)"
 ./redsocks
