4686d2e547e68fc3e322343f2859712d03a7d2ad
Soichi Hayashi <soichih@gmail.com>
2015-01-17 17:35:37 +0000

    created redsocks.init that can start/stop both redsocks and ssh tunnel

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "DEPRECATED use redsocks.ini script instead!"
+
 echo "starting socks5 proxy"
 ssh  -o StrictHostKeyChecking=no -fqND localhost:1080 -i secrets/sock5_proxy.id_rsa root@maddash.aglt2.org
 echo $! > /var/run/pf-socks5-ssh.pid
